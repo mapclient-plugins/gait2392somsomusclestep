10555e21dfb56fa384b910c20a80473b92f5d5cc
TimmyNic <tim.nicolas@outlook.com>
2021-08-03 10:30:07 +1200

    Updated method for checking package dependencies.

--- setup.py
+++ setup.py
@@ -1,21 +1,28 @@
 from setuptools import setup, find_packages
-import sys, os, io
+import io
 
-# List all of your Python package dependencies in the
-# requirements.txt file
 
-def readfile(filename):
+def readfile(filename, split=False):
     with io.open(filename, encoding="utf-8") as stream:
-        return stream.read().split("\n")
+        if split:
+            return stream.read().split("\n")
+        return stream.read()
 
-readme = readfile("README.rst")[3:]  # skip title
-requires = readfile("requirements.txt")
-license = readfile("LICENSE")
+
+package_readme = readfile("README.rst")[3:]  # skip title
+package_license = readfile("LICENSE")
+package_dependencies = [
+    "setuptools",
+    "PySide2",
+    "numpy",
+    "gias2",
+    "scipy",
+]
 
 setup(name=u'mapclientplugins.gait2392somsomusclestep',
       version='0.1',
       description='',
-      long_description=readme + license,
+      long_description='\n'.join(package_readme) + package_license,
       classifiers=[],
       author=u'Ju Zhang',
       author_email='',
@@ -25,5 +32,5 @@ setup(name=u'mapclientplugins.gait2392somsomusclestep',
       namespace_packages=['mapclientplugins'],
       include_package_data=True,
       zip_safe=False,
-      install_requires=requires,
+      install_requires=package_dependencies,
       )
