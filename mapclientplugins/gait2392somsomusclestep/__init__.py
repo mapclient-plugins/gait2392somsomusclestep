817f13a9008dbf3927a3a3bc2cafc52536ea151f
TimmyNic <tim.nicolas@outlook.com>
2022-12-19 17:51:28 +1300

    Set version to 0.3.0

--- mapclientplugins/gait2392somsomusclestep/__init__.py
+++ mapclientplugins/gait2392somsomusclestep/__init__.py
@@ -2,7 +2,7 @@
 MAP Client Plugin
 """
 
-__version__ = '0.2.1'
+__version__ = '0.3.0'
 __author__ = 'Ju Zhang'
 __stepname__ = 'Fieldwork Gait2392 SOMSO Muscles'
 __location__ = 'https://github.com/mapclient-plugins/fieldworkgait2392somsomusclestep/commits/master.zip'
